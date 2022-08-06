c3c672e7b5b6f25c024b8e2334c4e0db48f7e9f8
Chetan Taneja <99343930+ctaneja10@users.noreply.github.com>
2022-08-06 14:23:47 +0530

    Update settings.py

--- iCoder/iCoder/settings.py
+++ iCoder/iCoder/settings.py
@@ -18,8 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6n_$vkp0*j0o0ja=3pus_hz$4()l-z=q^jc_#d#)m84l^xdwa*'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
